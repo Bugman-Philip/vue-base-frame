ec99b5d3baa9da135b94ce04bc5f5af75ccafa3a
songheng <songheng@cecbrain.com>
2020-08-17 16:51:23 +0800

    @bugman: eslintrc and environment configurate

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,41 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/standard',
-    '@vue/typescript/recommended'
-  ],
-  parserOptions: {
-    ecmaVersion: 2020
-  },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  },
-  overrides: [
-    {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)'
-      ],
-      env: {
-        jest: true
-      }
-    }
-  ]
+    root: true,
+    env: {
+        node: true
+    },
+    extends: [
+        'plugin:vue/essential',
+        '@vue/standard',
+        '@vue/typescript/recommended'
+    ],
+    parserOptions: {
+        ecmaVersion: 2020
+    },
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        indent: [2, 4],
+        '@typescript-eslint/no-var-requires': 0,
+        '@typescript-eslint/no-use-before-define': 0,
+        'class-methods-use-this': 'off',
+        'no-trailing-spaces': 0,
+        'max-len': [1, 150, 4],
+        'no-restricted-modules': [0],
+        'comma-dangle': [2, 'never'],
+        'spaced-comment': 0,
+        'arrow-body-style': ['error', 'as-needed', { requireReturnForObjectLiteral: true }],
+        'max-classes-per-file': ['error', 100],
+        '@typescript-eslint/ban-ts-ignore': 'off'
+    },
+    overrides: [
+        {
+            files: [
+                '**/__tests__/*.{j,t}s?(x)',
+                '**/tests/unit/**/*.spec.{j,t}s?(x)'
+            ],
+            env: {
+                jest: true
+            }
+        }
+    ]
 }
