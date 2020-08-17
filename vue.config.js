9d1e9a8cdd85c3d33d5dd1f889532f4ee2618fdd
songheng <songheng@cecbrain.com>
2020-08-17 17:53:47 +0800

    @bugman: eslitrc add semi trim

---
+++ vue.config.js
@@ -0,0 +1,38 @@
+const TerserPlugin = require('terser-webpack-plugin')
+
+module.exports = {
+    productionSourceMap: true,
+    configureWebpack: {
+        optimization: {
+            minimizer: [
+                new TerserPlugin({
+                    sourceMap: false, // Must be set to true if using source-maps in production
+                    terserOptions: {
+                        compress: {
+                            warnings: false,
+                            dropConsole: true,
+                            dropDebugger: true,
+                            pureFuncs: ['console.log']
+                        }
+                    }
+                })
+            ]
+        }
+    },
+
+    devServer: {
+        host: '0.0.0.0',
+        port: 8080,
+        https: false,
+        // 设置代理
+        proxy: {
+            '/api': {
+                target: 'http://192.168.1.17:9210/',
+                changOrigin: true,
+                pathRewrite: {
+                    '^/api': ''
+                }
+            }
+        }
+    }
+}
